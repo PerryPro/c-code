41c72d8acd4bd786d7e28d715989e776bb3f224f
PerryPro <41824503+PerryPro@users.noreply.github.com>
2018-09-26 20:49:36 +0800

    new
    new

---
+++ 人生问答.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <string.h>
+
+using namespace std;
+
+int main()
+
+{
+
+cout<<"你能放下曾经拥有的一切吗？"<<endl;
+
+string a;
+
+cin>>a;
+
+if(a=="能") 
+
+{
+
+cout<<"你永远属于你的梦想。(/≧▽≦/)"<<endl;
+
+}
+else if(a=="不能")
+{
+	cout<<"缺乏勇敢的人无法成功⊙﹏⊙";
+	
+	}
+else if(a=="不清楚")
+{
+	cout<<"你应该对自己有所认识←_←";
+	}
+else 
+{
+	
+	cout<<"你在搞笑吧(▼皿▼#)";
+	}
+
+}
